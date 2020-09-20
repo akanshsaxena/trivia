8666d8cd9a0c6249c3f16b9e5d81a58a8f969a12
unknown <53352640+akanshsaxena@users.noreply.github.com>
2020-09-20 18:54:44 +0530

    Correct/incorrect responses added

--- src/components/ShowQuestions.js
+++ src/components/ShowQuestions.js
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import DisplayQuestion from "./DisplayQuestion.js";
 
 export default function ShowQuestions(props) {
   const [index, setIndex] = useState(0);
   const [choice, setChoice] = useState("");
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [isWrong, setIsWrong] = useState(false);
+  const [correctAnswer, setCorrectAnswer] = useState(0);
   const array = props.data;
+
   const handleClick = (event) => {
     event.preventDefault();
     setIndex(index + 1);
+    setChoice("");
+    setIsCorrect(false);
+    setIsWrong(false);
   };
 
   let answers = [
@@ -15,22 +22,45 @@ export default function ShowQuestions(props) {
     array[index].correct_answer,
   ];
 
+  const getValue = (event) => {
+    setChoice(event.target.value);
+    console.log(choice);
+  };
+
   const checkAnswer = (event) => {
     event.preventDefault();
-    setChoice(event.target.value);
+    if (choice === array[index].correct_answer) {
+      console.log(`if ${isCorrect}`);
+      setIsCorrect(true);
+      setIsWrong(false);
+      setCorrectAnswer(correctAnswer + 1);
+    } else {
+      console.log(`else ${isCorrect}`);
+      setIsCorrect(false);
+      setIsWrong(true);
+    }
   };
+
   return (
     <div>
       <h3>{array[index].question}</h3>
       {answers.map((answer) => (
         <label>
-          <input type="radio" name="answer" value={answer} />
+          <input
+            type="radio"
+            name="answer"
+            value={answer}
+            checked={choice === answer}
+            onChange={getValue}
+          />
           {answer}
           <br />
         </label>
       ))}
       <button onClick={checkAnswer}>Submit</button>
-      <button onClick={handleClick}> Next </button>
+      {isCorrect && <p id="correct">Correct</p>}
+      {isWrong && <p id="incorrect">Incorrect</p>}
+      <button onClick={handleClick}>Next</button>
     </div>
   );
 }
